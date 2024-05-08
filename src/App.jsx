f365d63e42f3889eaa0d8f52f17dd91a5084e25d
aufarabul <aufarabul@gmail.com>
2024-05-08 21:53:22 +0700

    feature : login

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-import './App.css'
-import HomePage from './pages/HomePage'
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
 
 const router = createBrowserRouter([
   {
-    path : '/',
+    path: "/",
     element: (
       <>
-        <HomePage/>
+        <HomePage />
       </>
-    )
+    ),
   },
-])
+  {
+    path: "/login",
+    element: (
+      <>
+        <LoginPage />
+      </>
+    ),
+  },
+]);
 
 export default function App() {
-  return (
-    <RouterProvider router={router} />
-  )
+  return <RouterProvider router={router} />;
 }
