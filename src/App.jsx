8922ab3813b8f48affa935d52bb92458a54bd63b
hukmakun1 <156909741+hukmakun1@users.noreply.github.com>
2024-05-10 21:31:23 +0700

    feature : choice leader (#2)

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/register";
+import ChoiceLeaderPage from "./pages/choiceleader";
 import PollingPage from "./pages/polling";
 
 const router = createBrowserRouter([
@@ -28,6 +29,14 @@ const router = createBrowserRouter([
     element: (
       <>
         <RegisterPage />
+      </>
+    ),
+  },
+  {
+    path: "/choiceleader",
+    element: (
+      <>
+        <ChoiceLeaderPage />
       </>
     ),
   },
