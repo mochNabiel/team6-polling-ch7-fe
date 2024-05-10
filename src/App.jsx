fd8a70fd5444f8663f50ee4338b97c6db10756b0
dvlboo <kuhhhckr@gmail.com>
2024-05-10 21:29:06 +0700

    fix app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import "./App.css";
 import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/register";
+import PollingPage from "./pages/polling";
 
 const router = createBrowserRouter([
   {
@@ -14,12 +16,28 @@ const router = createBrowserRouter([
     )
   },
   {
+    path: "/login",
+    element: (
+      <>
+        <LoginPage />
+      </>
+    ),
+  },
+  {
     path: "/register",
     element: (
       <>
         <RegisterPage />
       </>
     )
+  },
+  {
+    path: "/polling",
+    element: (
+      <>
+        <PollingPage />
+      </>
+    ),
   },
 ]);
 
